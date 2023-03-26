580a93d53ca009f69c223b899cd688913f2effb4
zantaclaus <tarzuto@gmail.com>
2023-03-26 16:15:34 +0700

    🚀 chore(package.json): add react-router-dom dependency ✨ feat(App.tsx): add RouterProvider component 🎉 feat(pages): add About, Document, and Home pages 🎨 style(routes): create router using createBrowserRouter The react-router-dom dependency is added to the package.json file. The RouterProvider component is added to the App.tsx file, which allows the application to use the react-router-dom library. The About, Document, and Home pages are added to the pages directory. The router is created using createBrowserRouter in the index.tsx file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes';
+
 function App() {
-  return <h1>App</h1>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+import About from '@/pages/About';
+import Document from '@/pages/Document';
+import Home from '@/pages/Home';
+
+export const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: '/about', element: <About /> },
+  { path: '/document', element: <Document /> },
+  { path: '*', element: <div>Not Found</div> },
+]);
