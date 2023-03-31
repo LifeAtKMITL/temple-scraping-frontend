0a1fd580faa569df86f8ef7973050513e28cce4c
gandastik <gandastik@gmail.com>
2023-03-31 23:47:42 +0700

    ✨ feat: add context 🥺

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { RouterProvider } from 'react-router-dom';
 
 import { router } from './routes';
+import TempleProvider from './contexts/TempleProvider';
 
 function App() {
   return (
     <>
-      <RouterProvider router={router} />
+      {/* I don't know where to put this. 😢 */}
+      <TempleProvider>
+        <RouterProvider router={router} />
+      </TempleProvider>
     </>
   );
 }
---
+++ src/hooks/useTemple.tsx
@@ -0,0 +1,14 @@
+import TempleContext from '@/contexts/TempleContext';
+import { useContext } from 'react';
+
+const useTemple = () => {
+  const context = useContext(TempleContext);
+
+  if (!context) {
+    throw new Error('useTemple must be used within a TempleProvider');
+  }
+
+  return context;
+};
+
+export default useTemple;
