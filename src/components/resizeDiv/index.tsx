fa9dd78f3860006c564878982ecc931e56b54e16
zantaclaus <tarzuto@gmail.com>
2023-04-07 21:42:55 +0700

    🛠 Remove Logo

--- src/components/resizeDiv/index.tsx
+++ src/components/resizeDiv/index.tsx
@@ -9,9 +9,8 @@ interface ArrowProps {
 const ResizeDiv: React.FC<ArrowProps> = () => {
   return (
     <div className='flex px-5 rounded-r-xl bg-bg h-screen text-white'>
-      <div className='mr-2.5 relative'>
-        <p>LOGO</p>
-        <p>ค้นหาด้วยจังหวัดหรือชื่อวัด</p>
+      <div className='mr-2.5 relative pt-6'>
+        <p className='text-lg'>ค้นหาด้วยจังหวัดหรือชื่อวัด</p>
         <SearchBox />
         <Showmap />
       </div>
