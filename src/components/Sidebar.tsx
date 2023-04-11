8c34a66d0fe6b1605ca8592d434ddfcee2a4f1b6
zantaclaus <tarzuto@gmail.com>
2023-04-11 21:32:31 +0700

    🛠 Change group name

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,7 +4,7 @@ import { MapContainer } from './map';
 const Sidebar = () => {
   return (
     <div className='hidden xl:block pl-5 pr-10 py-10 rounded-r-xl bg-secondary h-screen text-white relative'>
-      <h1 className='text-xl sukhumvit-bold mb-8 pl-1'>กลุ่ม ณัฐนันท์สุดเฟี้ยวกับไอเสี่ยวทั้ง 10</h1>
+      <h1 className='text-xl sukhumvit-bold mb-8 pl-1'>กลุ่ม ณัฐนันท์สุดเฟี้ยวกับไอเสี่ยวทั้ง 9</h1>
       <SearchBox />
       <MapContainer />
     </div>
